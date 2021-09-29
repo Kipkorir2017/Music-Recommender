f6367a8c2599f983985a97b4ab8ce5fe190a8cd9
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-29 09:32:33 +0300

    styling registration form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app-register', component:RegisterComponent},
+  { path: 'app-navbar', component: NavbarComponent},
+  {path:'app-landing', component:LandingComponent},
+  {path:'app-login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
